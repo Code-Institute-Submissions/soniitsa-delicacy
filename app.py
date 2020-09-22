775e0af9ee9c04d1d38e49f04be08e486c1509d2
soniitsa <ojosonia@yahoo.com>
2020-09-22 19:02:31 +0000

    Commiting all the files

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'FoodRecipes'
+app.config["MONGO_URI"] = 'mongodb+srv://sonia:candy1991@myfirstproject.jnu5z.mongodb.net/FoodRecipes?retryWrites=true&w=majority'
 
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_recipe')
+def get_recipes():
+    data = []
 
 
 if __name__ == '__main__':
